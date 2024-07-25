8fd56a2d458463576e4f7c5f0b414b5f7c75afc6
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2024-07-25 07:01:41 +0200

    hotfix OCEL exports (consistency check)

--- pm4py/objects/ocel/exporter/sqlite/variants/pandas_exporter.py
+++ pm4py/objects/ocel/exporter/sqlite/variants/pandas_exporter.py
@@ -1,6 +1,7 @@
 from pm4py.objects.ocel.obj import OCEL
 from typing import Dict, Any, Optional
 from pm4py.objects.ocel.util import ocel_consistency
+from pm4py.objects.ocel.util import filtering_utils
 import os
 
 
@@ -26,6 +27,7 @@ def apply(ocel: OCEL, target_path: str, parameters: Optional[Dict[Any, Any]] = N
         os.remove(target_path)
 
     ocel = ocel_consistency.apply(ocel, parameters=parameters)
+    ocel = filtering_utils.propagate_relations_filtering(ocel, parameters=parameters)
 
     conn = sqlite3.connect(target_path)
 
